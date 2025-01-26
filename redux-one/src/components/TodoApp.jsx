e34c08e6f9b8d1b442fa1c671305c2b7bb566ae7
VladyBarvy <vladbarskoff@yandex.ru>
2025-01-26 18:16:13 +0300

    260125 add EntityAdapter

--- redux-one/src/components/TodoApp.jsx
+++ redux-one/src/components/TodoApp.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addTodo, removeTodo } from '../slices/todosSlice';
+import { selectAllTodos, selectTodoIds } from '../slices/todosSlice';
 
 const TodoApp = () => {
   const [newTodo, setNewTodo] = useState('');
-  const ids = useSelector((state) => state.todos.ids);  // Получаем ID задач
-  const entities = useSelector((state) => state.todos.entities);  // Получаем сами задачи
+
+
+  // const ids = useSelector((state) => state.todos.ids);  // Получаем ID задач
+  // const entities = useSelector((state) => state.todos.entities);  // Получаем сами задачи
+  const todos = useSelector(selectAllTodos); // Получаем все задачи через селектор
+
+
   const dispatch = useDispatch();
 
   const handleAddTodo = () => {
     if (newTodo.trim()) {
       dispatch(addTodo(newTodo));
       setNewTodo('');
+      console.log('boob: ', todos);
     }
   };
 
@@ -32,12 +39,22 @@ const TodoApp = () => {
         <button onClick={handleAddTodo}>Add Task</button>
       </div>
       <ul>
+        {/* 
         {ids.map((id) => (
           <li key={id}>
-            {entities[id].text}  {/* Получаем текст задачи по ID */}
+            {entities[id].text}  
             <button onClick={() => handleRemoveTodo(id)}>Delete</button>
           </li>
+        ))} 
+        */}
+
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text} {/* Выводим текст задачи */}
+            <button onClick={() => handleRemoveTodo(todo.id)}>Delete</button>
+          </li>
         ))}
+
       </ul>
     </div>
   );
--- redux-one/src/slices/todosSlice.js
+++ redux-one/src/slices/todosSlice.js
@@ -1,29 +1,54 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createEntityAdapter } from '@reduxjs/toolkit';
+
+// Создание адаптера для задач
+const todosAdapter = createEntityAdapter();
+
+
+
 
 // Начальное состояние с нормализованными данными
+/*
 const initialState = {
   entities: {},
   ids: [],
 };
+*/
+const initialState = todosAdapter.getInitialState();
+
+
+
 
 const todosSlice = createSlice({
   name: 'todos',
   initialState,
+  // reducers: {
+  //   addTodo: (state, { payload }) => {
+  //     const newTodo = {
+  //       id: Date.now(),
+  //       text: payload,
+  //     };
+  //     // Добавляем новый todo в entities и добавляем его ID в ids
+  //     state.entities[newTodo.id] = newTodo;
+  //     state.ids.push(newTodo.id);
+  //   },
+  //   removeTodo: (state, action) => {
+  //     const todoId = action.payload;
+  //     // Удаляем задачу из entities по ID и удаляем ID из ids
+  //     delete state.entities[todoId];
+  //     state.ids = state.ids.filter(id => id !== todoId);
+  //   },
+  // },
+
   reducers: {
-    addTodo: (state, { payload }) => {
-      const newTodo = {
+    addTodo: (state, action) => {
+      todosAdapter.addOne(state, {
         id: Date.now(),
-        text: payload,
-      };
-      // Добавляем новый todo в entities и добавляем его ID в ids
-      state.entities[newTodo.id] = newTodo;
-      state.ids.push(newTodo.id);
+        text: action.payload,
+      });
     },
     removeTodo: (state, action) => {
-      const todoId = action.payload;
-      // Удаляем задачу из entities по ID и удаляем ID из ids
-      delete state.entities[todoId];
-      state.ids = state.ids.filter(id => id !== todoId);
+      todosAdapter.removeOne(state, action.payload);
     },
   },
 });
@@ -31,3 +56,10 @@ const todosSlice = createSlice({
 export const { addTodo, removeTodo } = todosSlice.actions;
 
 export default todosSlice.reducer;
+
+// Экспортируем селекторы, созданные с помощью адаптера
+export const {
+  selectAll: selectAllTodos, // Получение всех задач
+  selectById: selectTodoById, // Получение задачи по ID
+  selectIds: selectTodoIds, // Получение списка всех ID
+} = todosAdapter.getSelectors(state => state.todos);
